89a536cf4d9ff469f257d2450871f1cba29230a0
VincenzoTavassi <info@egadivacanze.it>
2023-03-10 15:13:18 +0100

    base completa

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php 
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Lista Hotel</title>
+</head>
+<body>
+    <div class="container">
+<h1 class="text-center my-2">Lista Hotel</h1>
+
+<table class="table my-5">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza dal centro</th>
+      <th scope="col">Descrizione</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($hotels as $hotel) : ?>
+      <tr>
+        <th scope="row"> <?=$hotel["name"]?> </th>
+        <td><?=$hotel["parking"] ? 'Presente' : 'Assente' ?></td>
+        <td><?=$hotel["vote"]?></td>
+        <td><?=$hotel["distance_to_center"]?> km</td>
+        <td><?=$hotel["description"]?></td>
+        
+      </tr>
+      <?php endforeach ?>
+  </tbody>
+</table>
+
+    </div>
+</body>
+</html>
